73d20dbd76a41e65b886320507e63c6c234c5aed
Saurabh <saurabh.7teen@gmail.com>
2022-05-29 00:09:49 +0530

    added config two with ssh key pairs

---
+++ .terraform/providers/registry.terraform.io/hashicorp/aws/config-two/vars.tf
@@ -0,0 +1,24 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default = "ap-south-1"
+}
+
+variable "AMIS" {
+  type=map
+  default={
+      ap-south-1="ami-0f2e255ec956ade7f"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
